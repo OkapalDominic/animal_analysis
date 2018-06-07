0aec657715d3cf20b8d34e9ff6ec11960eba523c
djo <djo@pdx.edu>
2018-06-07 14:10:01 -0700

    Added TODO in presenter.py

--- presenter.py
+++ presenter.py
@@ -14,6 +14,7 @@ class Presenter:
     def analyze(self,data):
     	photo = data.files['file']
     	(labels,uri) = self.model.labelImage(photo)
+    	# TODO: Get sentiment from data.form['description'] and set feelings below to it.
     	image_data = dict(label=labels[0].description, feelings="really like", image=uri)
     	args = {'image_data':image_data}
     	route = Route(False, 'index.html', args)
